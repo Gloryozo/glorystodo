c297cde4ac1e80bd3b9b51369dd4c9ecd7e1ea1d
Gloryozo <gloryozoji@gmail.com>
2024-11-01 13:30:19 +0200

    created util.js file and modified codes for error handling

--- server/index.js
+++ server/index.js
@@ -2,56 +2,24 @@
 import express from 'express'; // Import the Express module for creating the server
 import cors from 'cors'; // Import CORS to allow cross-origin requests
 import { pool } from './helper/db.js'; // Import the pool object from the db.js file
+import myTodoRouter from './routes/myTodoRouter.js'; // Import the router object from the myTodoRouter.js file
 
 // Set the port number where the server will listen for requests
-const port = 3001; 
+const port = process.env.PORT 
 const app = express(); // Create an instance of an Express application
 
 // Middleware to enable CORS and parse incoming JSON and URL-encoded data
 app.use(cors()); // Allow requests from different origins
 app.use(express.json()); // Parse JSON data in requests
 app.use(express.urlencoded({ extended: false })); // Parse URL-encoded data
+app.use('/', myTodoRouter);
 
-// Define a GET route for the root URL
-app.get('/', (req, res) => {
-    // Query the database to get all tasks
-    pool.query('select * from mytask', (error, result) => {
-        if (error) {
-            // If there's an error, send a 500 status with the error message
-            return res.status(500).json({ error: error.message });
-        } 
-        // If successful, send a 200 status with the list of tasks
-        return res.status(200).json(result.rows);
-    });
+app.use((err,req,res,next) => {
+    const statusCode = err.statusCode || 500;
+    console.error(err.stack);
+    res.status(statusCode).json({error: err.message});
 });
 
-// Define a POST route to create a new task
-app.post('/create', (req, res) => {
-  // Insert a new task into the database
-    pool.query('insert into mytask (description) values ($1) returning *', [req.body.description], (error, result) => {
-        if (error) {
-            // If there's an error, send a 500 status with the error message
-            return res.status(500).json({ error: error.message });
-        }
-        // If successful, send a 201 status with the ID of the new task
-        return res.status(201).json({ id: result.rows[0].id });
-    });
-});
-
-// Define a DELETE route to remove a task by ID
-app.delete('/delete/:id', (req, res) => {
-    const id = parseInt(req.params.id); // Get the ID from the request parameters and convert it to an integer
-
-    // Delete the task with the specified ID from the database
-    pool.query('delete from mytask where id=$1', [id], (error, result) => {
-        if (error) {
-            // If there's an error, send a 500 status with the error message
-            return res.status(500).json({ error: error.message });
-        }
-        // If successful, send a 200 status with the deleted task's ID
-        return res.status(200).json({ id: req.params.id });
-    });
-});
 
 // Start the server and listen for incoming requests
 app.listen(port, () => {
--- server/routes/myTodoRouter.js
+++ server/routes/myTodoRouter.js
@@ -1,39 +1,40 @@
 import { pool } from '../helper/db.js'; // Import the pool object from the db.js file   
 import { Router } from 'express';
+import { emptyOrRows } from '../helper/utils.js';
 
 const router = Router(); // Create a new router
 
-router.get('/', async (req, res) => {
+router.get('/', async (req, res, next) => {
     pool.query('select * from mytask', (error, result) => {
         if (error) {
             // If there's an error, send a 500 status with the error message
-            return res.status(500).json({ error: error.message });
+            return next(error)
         } 
         // If successful, send a 200 status with the list of tasks
-        return res.status(200).json(result.rows);
+        return res.status(200).json(emptyOrRows(result));
     });
 });
 
-router.post('create', async (req, res) => {
+router.post('create', async (req, res, next) => {
     // Insert a new task into the database
     pool.query('insert into mytask (description) values ($1) returning *', [req.body.description], (error, result) => {
         if (error) {
             // If there's an error, send a 500 status with the error message
-            return res.status(500).json({ error: error.message });
+            return next(error)
         }
         // If successful, send a 201 status with the ID of the new task
         return res.status(201).json({ id: result.rows[0].id });
     });
 });
 
-router.delete('delete/:id', async (req, res) => {
+router.delete('delete/:id', async (req, res, next) => {
     const id = parseInt(req.params.id); // Get the ID from the request parameters and convert it to an integer
 
     // Delete the task with the specified ID from the database
     pool.query('delete from mytask where id=$1', [id], (error, result) => {
         if (error) {
             // If there's an error, send a 500 status with the error message
-            return res.status(500).json({ error: error.message });
+            return next(error)
         }
         // If successful, send a 200 status with the deleted task's ID
         return res.status(200).json({ id: req.params.id });
